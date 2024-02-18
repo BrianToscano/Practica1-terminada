213fdd6af70f2375df3e81475ba4c4dd84d0815e
BrianToscano <BrianToscano@DESKTOP-DTTQ49M>
2024-02-17 20:58:40 -0800

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Practica1
+{
+    partial class lbl_x2
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Btn_resolver = new System.Windows.Forms.Button();
+            this.lbl_titulo = new System.Windows.Forms.Label();
+            this.lbl_Ecuacion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbx_a = new System.Windows.Forms.TextBox();
+            this.tbx_b = new System.Windows.Forms.TextBox();
+            this.tbx_c = new System.Windows.Forms.TextBox();
+            this.Btn_salir = new System.Windows.Forms.Button();
+            this.lbl_1 = new System.Windows.Forms.Label();
+            this.lbl_2 = new System.Windows.Forms.Label();
+            this.lbl_a = new System.Windows.Forms.Label();
+            this.lbl_b = new System.Windows.Forms.Label();
+            this.lbl_c = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Btn_resolver
+            // 
+            this.Btn_resolver.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_resolver.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Btn_resolver.Location = new System.Drawing.Point(203, 215);
+            this.Btn_resolver.Name = "Btn_resolver";
+            this.Btn_resolver.Size = new System.Drawing.Size(337, 58);
+            this.Btn_resolver.TabIndex = 0;
+            this.Btn_resolver.Text = "Resolver";
+            this.Btn_resolver.UseVisualStyleBackColor = true;
+            this.Btn_resolver.Click += new System.EventHandler(this.Btn_resolver_Click);
+            // 
+            // lbl_titulo
+            // 
+            this.lbl_titulo.AutoSize = true;
+            this.lbl_titulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_titulo.Location = new System.Drawing.Point(286, 45);
+            this.lbl_titulo.Name = "lbl_titulo";
+            this.lbl_titulo.Size = new System.Drawing.Size(200, 24);
+            this.lbl_titulo.TabIndex = 1;
+            this.lbl_titulo.Text = "Ecuacion cuadratica";
+            // 
+            // lbl_Ecuacion
+            // 
+            this.lbl_Ecuacion.AutoSize = true;
+            this.lbl_Ecuacion.Font = new System.Drawing.Font("Arial", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_Ecuacion.Location = new System.Drawing.Point(193, 107);
+            this.lbl_Ecuacion.Name = "lbl_Ecuacion";
+            this.lbl_Ecuacion.Size = new System.Drawing.Size(378, 55);
+            this.lbl_Ecuacion.TabIndex = 5;
+            this.lbl_Ecuacion.Text = "x   +       x+     =0";
+            this.lbl_Ecuacion.Click += new System.EventHandler(this.label_Ecuacion_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(228, 107);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "2";
+            // 
+            // tbx_a
+            // 
+            this.tbx_a.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_a.Location = new System.Drawing.Point(157, 100);
+            this.tbx_a.Name = "tbx_a";
+            this.tbx_a.Size = new System.Drawing.Size(42, 62);
+            this.tbx_a.TabIndex = 7;
+            this.tbx_a.Text = "9";
+            this.tbx_a.TextChanged += new System.EventHandler(this.tbx_a_TextChanged);
+            // 
+            // tbx_b
+            // 
+            this.tbx_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_b.Location = new System.Drawing.Point(322, 100);
+            this.tbx_b.Name = "tbx_b";
+            this.tbx_b.Size = new System.Drawing.Size(49, 62);
+            this.tbx_b.TabIndex = 8;
+            this.tbx_b.Text = "3";
+            // 
+            // tbx_c
+            // 
+            this.tbx_c.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbx_c.Location = new System.Drawing.Point(447, 100);
+            this.tbx_c.Name = "tbx_c";
+            this.tbx_c.Size = new System.Drawing.Size(39, 62);
+            this.tbx_c.TabIndex = 9;
+            this.tbx_c.Text = "2";
+            // 
+            // Btn_salir
+            // 
+            this.Btn_salir.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Btn_salir.Location = new System.Drawing.Point(662, 381);
+            this.Btn_salir.Name = "Btn_salir";
+            this.Btn_salir.Size = new System.Drawing.Size(112, 57);
+            this.Btn_salir.TabIndex = 10;
+            this.Btn_salir.Text = "Salir";
+            this.Btn_salir.UseVisualStyleBackColor = true;
+            this.Btn_salir.Click += new System.EventHandler(this.Btn_salir_Click);
+            // 
+            // lbl_1
+            // 
+            this.lbl_1.AutoSize = true;
+            this.lbl_1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_1.Location = new System.Drawing.Point(197, 312);
+            this.lbl_1.Name = "lbl_1";
+            this.lbl_1.Size = new System.Drawing.Size(58, 31);
+            this.lbl_1.TabIndex = 11;
+            this.lbl_1.Text = "x1=";
+            // 
+            // lbl_2
+            // 
+            this.lbl_2.AutoSize = true;
+            this.lbl_2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_2.Location = new System.Drawing.Point(197, 363);
+            this.lbl_2.Name = "lbl_2";
+            this.lbl_2.Size = new System.Drawing.Size(58, 31);
+            this.lbl_2.TabIndex = 12;
+            this.lbl_2.Text = "x2=";
+            // 
+            // lbl_a
+            // 
+            this.lbl_a.AutoSize = true;
+            this.lbl_a.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_a.Location = new System.Drawing.Point(38, 273);
+            this.lbl_a.Name = "lbl_a";
+            this.lbl_a.Size = new System.Drawing.Size(52, 31);
+            this.lbl_a.TabIndex = 13;
+            this.lbl_a.Text = "a =";
+            // 
+            // lbl_b
+            // 
+            this.lbl_b.AutoSize = true;
+            this.lbl_b.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_b.Location = new System.Drawing.Point(39, 321);
+            this.lbl_b.Name = "lbl_b";
+            this.lbl_b.Size = new System.Drawing.Size(52, 31);
+            this.lbl_b.TabIndex = 14;
+            this.lbl_b.Text = "b =";
+            // 
+            // lbl_c
+            // 
+            this.lbl_c.AutoSize = true;
+            this.lbl_c.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_c.Location = new System.Drawing.Point(40, 363);
+            this.lbl_c.Name = "lbl_c";
+            this.lbl_c.Size = new System.Drawing.Size(51, 31);
+            this.lbl_c.TabIndex = 15;
+            this.lbl_c.Text = "c =";
+            // 
+            // lbl_x2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbl_c);
+            this.Controls.Add(this.lbl_b);
+            this.Controls.Add(this.lbl_a);
+            this.Controls.Add(this.lbl_2);
+            this.Controls.Add(this.lbl_1);
+            this.Controls.Add(this.Btn_salir);
+            this.Controls.Add(this.tbx_c);
+            this.Controls.Add(this.tbx_b);
+            this.Controls.Add(this.tbx_a);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbl_Ecuacion);
+            this.Controls.Add(this.lbl_titulo);
+            this.Controls.Add(this.Btn_resolver);
+            this.Name = "lbl_x2";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Btn_resolver;
+        private System.Windows.Forms.Label lbl_titulo;
+        private System.Windows.Forms.Label lbl_Ecuacion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbx_a;
+        private System.Windows.Forms.TextBox tbx_b;
+        private System.Windows.Forms.TextBox tbx_c;
+        private System.Windows.Forms.Button Btn_salir;
+        private System.Windows.Forms.Label lbl_1;
+        private System.Windows.Forms.Label lbl_2;
+        private System.Windows.Forms.Label lbl_a;
+        private System.Windows.Forms.Label lbl_b;
+        private System.Windows.Forms.Label lbl_c;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Practica1
+{
+    public partial class lbl_x2 : Form
+    {
+        private double disc;
+
+        public lbl_x2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label_Ecuacion_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_resolver_Click(object sender, EventArgs e)
+        {
+            double a, b, c, x1, x2;
+            // Validar la entrada para a
+            if (!double.TryParse(tbx_a.Text, out a))
+            {
+                MessageBox.Show("Ingrese un valor numérico para a", "Error");
+                tbx_a.Focus();
+                return;
+            }
+            // Validar la entrada para b
+            if (!double.TryParse(tbx_b.Text, out b))
+            {
+                MessageBox.Show("Ingrese un valor numérico para b", "Error");
+                tbx_b.Focus();
+                return;
+            }
+            // Validar la entrada para c
+            if (!double.TryParse(tbx_c.Text, out c))
+            {
+                MessageBox.Show("Ingrese un valor numérico para c", "Error");
+                tbx_c.Focus();
+                return;
+            }
+            // Calcular el discriminante
+            double discriminante = Math.Pow(b, 2) - 4 * a * c;
+            // Calcular las raíces de la ecuación cuadrática
+            if (discriminante > 0)
+            {
+                x1 = (-b + Math.Sqrt(discriminante)) / (2 * a);
+                x2 = (-b - Math.Sqrt(discriminante)) / (2 * a);
+                lbl_1.Text = "x1 = " + x1.ToString();
+                lbl_2.Text = "x2 = " + x2.ToString();
+                
+ }
+            else if (discriminante == 0)
+            {
+                x1 = -b / (2 * a);
+                lbl_1.Text = "x1 = x2 = " + x1.ToString();
+                lbl_2.Text = "";
+            }
+            else
+            {
+                // Calcular la parte real e imaginaria de las raíces complejas
+                double parteReal = -b / (2 * a);
+                double parteImaginaria = Math.Sqrt(-discriminante) / (2 * a);
+                lbl_1.Text = "x1 = " + parteReal.ToString("F2") + " + " +
+               parteImaginaria.ToString("F2") + "i";
+                lbl_2.Text = "x2 = " + parteReal.ToString("F2") + " - " +
+               parteImaginaria.ToString("F2") + "i";
+            }
+            lbl_a.Text = Convert.ToString(a);
+            lbl_b.Text = Convert.ToString(b);
+            lbl_c.Text = Convert.ToString(c);
+        }
+
+
+        private void Btn_salir_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void tbx_a_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
